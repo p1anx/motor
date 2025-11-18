325bb0a8b8e8d31a7878994f147f162f7855f2ac
tuf-ubuntu <2514034568@qq.com>
2025-11-18 13:03:32 +0800

    add velocity degrees, but it can't be used

---
+++ simpleFOC_cmake_clion_h750vbt6_v2.4_20251117/BSP/FOC/pwm.h
@@ -0,0 +1,12 @@
+#ifndef __PWM_H
+#define __PWM_H
+#include "tim.h"
+
+extern TIM_HandleTypeDef htim1;
+#define pwm_tim htim1
+
+// void pwm_init(void);
+void pwm_init(int frequency, int resolution);
+int PWM6_Init(int frequency, int resolution); //6 pwm
+int test_PWM(int frequency, int resolution);
+#endif // !__PWM_H
